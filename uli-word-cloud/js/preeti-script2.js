6862ea3655b76fc69356ebbf79d5cb8a33bdb24a
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-11-27 16:45:45 +0530

    change mouse event listener

--- uli-word-cloud/js/preeti-script2.js
+++ uli-word-cloud/js/preeti-script2.js
@@ -1,10 +1,34 @@
-// let container = document.getElementById("container");
-let container= document.querySelector("#container")
-container.addEventListener("mousemove", (e) => {
-    const X = e.clientX;
-   const Y = e.clientY;
-   container.style.left = `${X - 25}px`; // 
-   container.style.top = `${Y- 25}px`
-   console.log("Hello")
- })
- 
+let container = document.getElementById("container");
+let isDragging = false;
+let offsetX, offsetY
+container.addEventListener("mousedown", (e) => {
+  isDragging = true;
+  offsetX = e.clientX - container.offsetLeft
+  offsetY = e.clientY - container.offsetTop
+  container.style.cursor = 'dragging'
+
+  container.addEventListener("mousemove", (e) => {
+    if (isDragging) {
+      const X = e.clientX;
+      const Y = e.clientY;
+      container.style.left = `${X - offsetX}px`; // change position of div
+      container.style.top = `${Y - offsetY}px`
+      // console.log("Hello")
+
+      container.addEventListener("mouseup", () => {
+        isDragging = false;
+        container.style.cursor = 'pointer'
+      })
+    }
+  })
+
+})
+
+
+
+
+
+
+
+
+
