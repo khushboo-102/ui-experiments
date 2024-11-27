26851d5a591a2b45a7ec42bd1cdbbd368d18acad
Manisha Kumari <manisha.tkg08@gmail.com>
2024-11-27 16:20:58 +0530

    Refactor positioning calculation to use dynamic values based on div

--- uli-word-cloud/js/script3.js
+++ uli-word-cloud/js/script3.js
@@ -5,12 +5,17 @@ mainContainer.addEventListener("mousedown", (e) => {
     isDragging = true; 
 });
 
+let width = parseInt(mainContainer.style.width)
+let height = parseInt(mainContainer.style.height)
+// console.log(height/2);
+// console.log(width/2)
+
 document.addEventListener("mousemove", (e) => {
     if (isDragging) { 
         const x = e.clientX;
         const y = e.clientY;
-        mainContainer.style.left = `${x - 25}px`;
-        mainContainer.style.top = `${y - 25}px`;
+        mainContainer.style.left = `${x - width/2}px`;
+        mainContainer.style.top = `${y - width/2}px`;
     }
 });
 
