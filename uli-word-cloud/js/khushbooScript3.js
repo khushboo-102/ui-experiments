01aa02928af76c4dcb326e653241b9e8c6e8a8da
Khushboo <khushbooatwork30@gmail.com>
2024-11-27 16:52:35 +0530

    changes in khushbooScript3.js file

--- uli-word-cloud/js/khushbooScript3.js
+++ uli-word-cloud/js/khushbooScript3.js
@@ -1,8 +1,31 @@
 let div = document.querySelector("#smallDiv")
-smallDiv.addEventListener("mousemove", (e) => {
-    console.log("Hello")
-    const X = e.clientX
-    const Y = e.clientY
-    smallDiv.style.left = `${X - 25}px`
-    smallDiv.style.top = `${Y - 25}px`
-})
+ 
+let isDragging = false;  
+let offsetX, offsetY;  // Store mouse position 
+
+// Mouse down event to start dragging
+smallDiv.addEventListener('mousedown', (e) => {
+    isDragging = true;  // Start dragging
+
+     offsetX = e.clientX - smallDiv.offsetLeft;
+    offsetY = e.clientY - smallDiv.offsetTop;
+
+    smallDiv.style.cursor = 'gradding';  // Change cursor to grabbing when dragging
+});
+
+// Mouse move event to move the div, only if dragging is true
+smallDiv.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        const X = e.clientX;
+        const Y = e.clientY;
+
+        smallDiv.style.left = `${X - offsetX}px`;  // Update left position
+        smallDiv.style.top = `${Y - offsetY}px`;  // Update top position
+    }
+});
+
+// Mouse up event to stop dragging
+smallDiv.addEventListener('mouseup', () => {
+    isDragging = false;  // Stop dragging
+    smallDiv.style.cursor = 'poiner';  // Reset cursor to normal pointer
+});
