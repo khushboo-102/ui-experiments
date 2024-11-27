1e5856b6e63993987dc6c394889931b20550dc69
Manisha Kumari <manisha.tkg08@gmail.com>
2024-11-27 15:13:13 +0530

    Add functionality to enable div movement only on click and drag

--- uli-word-cloud/js/script3.js
+++ uli-word-cloud/js/script3.js
@@ -1,8 +1,19 @@
 const mainContainer = document.querySelector("#main-container");
+let isDragging = false; 
 
-mainContainer.addEventListener("mousemove", (e) => {
-    const x = e.clientX;
-    const y = e.clientY;
-    mainContainer.style.left = `${x - 25}px`; 
-    mainContainer.style.top = `${y - 25}px`; 
+mainContainer.addEventListener("mousedown", (e) => {
+    isDragging = true; 
+});
+
+document.addEventListener("mousemove", (e) => {
+    if (isDragging) { 
+        const x = e.clientX;
+        const y = e.clientY;
+        mainContainer.style.left = `${x - 25}px`;
+        mainContainer.style.top = `${y - 25}px`;
+    }
+});
+
+document.addEventListener("mouseup", () => {
+    isDragging = false; 
 });
