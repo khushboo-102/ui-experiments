007f2698b9a3a7cd79ecd1796f3aad6c9bd97e4f
Preeti Aarya <kumaripreetiaarya00@gmail.com>
2024-11-28 17:53:19 +0530

    Preeti branch (#10)
    div across divs: preeti

--- uli-word-cloud/js/preeti-script2.js
+++ uli-word-cloud/js/preeti-script2.js
@@ -1,34 +1,56 @@
-let container = document.getElementById("container");
+let dragDiv = document.getElementById("drag-div");
+const categoryDivs = document.querySelectorAll(".category-div");
 let isDragging = false;
-let offsetX, offsetY
-container.addEventListener("mousedown", (e) => {  //start dragging 
+let offsetX, offsetY;
+let currentDiv = null;
+
+// -----------------Start dragging-------------------------------
+
+dragDiv.addEventListener("mousedown", (e) => {
   isDragging = true;
-  offsetX = e.clientX - container.offsetLeft
-  offsetY = e.clientY - container.offsetTop
-  container.style.cursor = 'dragging'
+  offsetX = e.clientX - dragDiv.offsetLeft;
+  offsetY = e.clientY - dragDiv.offsetTop;
+  dragDiv.style.cursor = "grabbing";
+});
 
-  container.addEventListener("mousemove", (e) => { // move container while the mouse help down
-    if (isDragging) {
-      const X = e.clientX;
-      const Y = e.clientY;
-      container.style.left = `${X - offsetX}px`; // change position of div
-      container.style.top = `${Y - offsetY}px`
-      // console.log("Hello")
+document.addEventListener("mousemove", (e) => {
+  if (isDragging) {
+    const X = e.clientX;
+    const Y = e.clientY;
+    dragDiv.style.left = `${X - offsetX}px`;
+    dragDiv.style.top = `${Y - offsetY}px`;
 
-      container.addEventListener("mouseup", () => { //end dragging
-        isDragging = false;
-        container.style.cursor = 'grab'
-      })
-    }
-  })
+    categoryDivs.forEach((categoryDiv) => {
+      const dragRect = dragDiv.getBoundingClientRect();
+      const categoryDivRect = categoryDiv.getBoundingClientRect();
 
-})
+      const overlay = !(
+        dragRect.right < categoryDivRect.left ||
+        dragRect.left > categoryDivRect.right ||
+        dragRect.bottom < categoryDivRect.top ||
+        dragRect.top > categoryDivRect.bottom
+      );
 
+      if (overlay) {
+        categoryDiv.style.backgroundColor = "yellow";
+        currentDiv = categoryDiv;
+      } else {
+        categoryDiv.style.backgroundColor = "";
+      }
+    });
+  }
+});
+document.addEventListener("mouseup", () => {
+  if (isDragging && currentDiv) {
+    const currentDivRect = currentDiv.getBoundingClientRect();
+    const centerX = currentDivRect.width / 2 - dragDiv.offsetWidth / 2;
+    const centerY = currentDivRect.height / 2 - dragDiv.offsetHeight / 2;
+    dragDiv.style.position = "absolute"; 
+    dragDiv.style.left = `${centerX}px`;
+    dragDiv.style.top = `${centerY}px`;
+    currentDiv.appendChild(dragDiv);
+  }
 
-
-
-
-
-
-
-
+  isDragging = false;
+  dragDiv.style.cursor = "grab";
+});
