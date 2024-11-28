a0884e1643546ac5f7fa1caeabbd066a28f2af35
Khushboo <khushbooatwork30@gmail.com>
2024-11-28 15:14:16 +0530

    added changes in khushbooIndex.html khushbooStyle.css khushbooScript3.js files

--- uli-word-cloud/js/khushbooScript3.js
+++ uli-word-cloud/js/khushbooScript3.js
@@ -1,31 +1,60 @@
-let div = document.querySelector("#smallDiv")
+let dragDiv = document.querySelector("#dragg-div")
+let categoriesDiv = document.querySelectorAll(".category-div")
  
 let isDragging = false;  
 let offsetX, offsetY;  // Store mouse position 
 
 // Mouse down event to start dragging
-smallDiv.addEventListener('mousedown', (e) => {
+dragDiv.addEventListener('mousedown', (e) => {
     isDragging = true;  // Start dragging
 
-     offsetX = e.clientX - smallDiv.offsetLeft;
-    offsetY = e.clientY - smallDiv.offsetTop;
+     offsetX = e.clientX - dragDiv.offsetLeft;
+    offsetY = e.clientY - dragDiv.offsetTop;
 
-    smallDiv.style.cursor = 'grabbing';  // Change cursor to grabbing when dragging
+    dragDiv.style.cursor = 'grabbing';  // Change cursor to grabbing when dragging
 });
 
 // Mouse move event to move the div, only if dragging is true
-smallDiv.addEventListener('mousemove', (e) => {
+dragDiv.addEventListener('mousemove', (e) => {
     if (isDragging) {
         const X = e.clientX;
         const Y = e.clientY;
 
-        smallDiv.style.left = `${X - offsetX}px`;  // Update left position
-        smallDiv.style.top = `${Y - offsetY}px`;  // Update top position
-    }
-});
+        dragDiv.style.left = `${X - offsetX}px`;  // Update left position
+        dragDiv.style.top = `${Y - offsetY}px`;  // Update top position
+        categoriesDiv.forEach(categoryDiv=> {
+            const div1 = dragDiv.getBoundingClientRect();
+            const div2 = categoryDiv.getBoundingClientRect();
+        
+            const overlay = !(
+              div1.right < div2.left ||
+              div1.left > div2.right ||
+              div1.bottom < div2.top ||
+              div1.top > div2.bottom
+            );
+          // Check for overlap and change color accordingly
+          if (overlay) {
+            console.log(categoryDiv);
+            
+            categoryDiv.style.backgroundColor = "yellow";
+          }else {
+            categoryDiv.style.backgroundColor = "white";
+          }
+          })
+    
+      }});
+    
+    
+    
+
 
 // Mouse up event to stop dragging
-smallDiv.addEventListener('mouseup', () => {
+dragDiv.addEventListener('mouseup', () => {
     isDragging = false;  // Stop dragging
-    smallDiv.style.cursor = 'poiner';  // Reset cursor to normal pointer
+    dragDiv.style.cursor = 'poiner';  // Reset cursor to normal pointer
 });
+ 
+    
+    
+ 
+    
